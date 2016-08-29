ab31664b1cc85b47dbd4d8d90511e070f408d67d
michal-majchrzycki <m.majchrzycki@gmail.com>
2016-08-29 23:24:23 +0200

    settings tab

--- Breakout/SettingsTableViewController.swift
+++ Breakout/SettingsTableViewController.swift
@@ -15,84 +15,68 @@ class SettingsTableViewController: UITableViewController {
             didSet {
                 let gravityValue = Float(UserSettings.sharedInstance.gravity)
                 gravitySlider.value = gravityValue
-                gravityLabel.text = String(format: "%.2f", gravityValue)
             }
         }
-        @IBOutlet weak var gravityLabel: UILabel!
-        @IBAction func changeGravity(sender: UISlider) {
+
+    @IBAction func changeGravity(sender: UISlider) {
             let gravityValue = CGFloat(sender.value)
             UserSettings.sharedInstance.gravity = gravityValue
-            gravityLabel.text = String(format: "%.2f", gravityValue)
             NSNotificationCenter.defaultCenter().postNotificationName("BreakoutViewControllerUpdateSettings", object: nil)
         }
         
-        // Elasticity
-        @IBOutlet weak var elasticitySlider: UISlider! {
+        @IBOutlet weak var elasticSlider: UISlider! {
             didSet {
                 let elasticityValue = Float(UserSettings.sharedInstance.elasticity)
-                elasticitySlider.value = elasticityValue
-                elasticityLabel.text = String(format: "%.2f", elasticityValue)
+                elasticSlider.value = elasticityValue
             }
         }
-        @IBOutlet weak var elasticityLabel: UILabel!
-        @IBAction func changeElasticity(sender: UISlider) {
+        @IBAction func changeElastic(sender: UISlider) {
             let elasticityValue = CGFloat(sender.value)
             UserSettings.sharedInstance.elasticity = elasticityValue
-            elasticityLabel.text = String(format: "%.2f", elasticityValue)
             NSNotificationCenter.defaultCenter().postNotificationName("BreakoutViewControllerUpdateSettings", object: nil)
         }
         
-        // Number of balls
-        @IBOutlet weak var numberOfBallsSegmentedControl: UISegmentedControl! {
+        @IBOutlet weak var numberOfBalls: UISegmentedControl! {
             didSet {
                 let numberOfBouncingBalls = UserSettings.sharedInstance.numberOfBalls
-                numberOfBallsSegmentedControl.selectedSegmentIndex = numberOfBouncingBalls - 1
+                numberOfBalls.selectedSegmentIndex = numberOfBouncingBalls - 1
             }
         }
-        @IBAction func changeNumberOfBalls(sender: UISegmentedControl) {
+        @IBAction func changeBalls(sender: UISegmentedControl) {
             UserSettings.sharedInstance.numberOfBalls = sender.selectedSegmentIndex + 1
         }
         
-        // Number of total bricks
         @IBOutlet weak var numberOfTotalBricksStepper: UIStepper! {
             didSet {
                 let numberOfTotalBricks = UserSettings.sharedInstance.numberOfTotalBricks
                 numberOfTotalBricksStepper.value = Double(numberOfTotalBricks)
-                numberOfTotalBricksLabel.text = "\(numberOfTotalBricks)"
             }
         }
-        @IBOutlet weak var numberOfTotalBricksLabel: UILabel!
+    
         @IBAction func changeNumberOfTotalBricks(sender: UIStepper) {
             let numberOfTotalBricks = Int(sender.value)
             UserSettings.sharedInstance.numberOfTotalBricks = numberOfTotalBricks
-            numberOfTotalBricksLabel.text = "\(numberOfTotalBricks)"
             
             if numberOfTotalBricks < UserSettings.sharedInstance.numberOfSpecialBricks {
                 UserSettings.sharedInstance.numberOfSpecialBricks = numberOfTotalBricks
                 numberOfSpecialBricksStepper.value = Double(numberOfTotalBricks)
-                numberOfSpecialBricksLabel.text = "\(numberOfTotalBricks)"
             }
             numberOfSpecialBricksStepper.maximumValue = Double(numberOfTotalBricks)
         }
         
-        // Number of special bricks
         @IBOutlet weak var numberOfSpecialBricksStepper: UIStepper! {
             didSet {
                 numberOfSpecialBricksStepper.maximumValue = Double(UserSettings.sharedInstance.numberOfTotalBricks)
                 
                 let numberOfSpecialBricks = UserSettings.sharedInstance.numberOfSpecialBricks
                 numberOfSpecialBricksStepper.value = Double(numberOfSpecialBricks)
-                numberOfSpecialBricksLabel.text = "\(numberOfSpecialBricks)"
             }
         }
-        @IBOutlet weak var numberOfSpecialBricksLabel: UILabel!
         @IBAction func changeNumberOfSpecialBricks(sender: UIStepper) {
             let numberOfSpecialBricks = Int(sender.value)
             UserSettings.sharedInstance.numberOfSpecialBricks = numberOfSpecialBricks
-            numberOfSpecialBricksLabel.text = "\(numberOfSpecialBricks)"
         }
         
-        // Special bricks
         @IBOutlet weak var brickSmallerPaddleSwitch: UISwitch! {
             didSet {
                 brickSmallerPaddleSwitch.on = UserSettings.sharedInstance.specialBrickSmallerPaddleEnabled
